95703586de77d0dd889d7249cde7f7201c1f7306
Antti Sivula <antti.j.sivula@gmail.com>
2025-01-06 23:20:17 +0200

    add code for 4.2

--- scanner.go
+++ scanner.go
@@ -2,6 +2,69 @@ package glox
 
 import "fmt"
 
+type TokenType int
+
+const (
+	// single-character tokens
+	LEFT_PAREN TokenType = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+
+	// one or two character tokens
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+
+	// literals
+	IDENTIFIER
+	STRING
+	NUMBER
+
+	// keywords
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+
+	EOF
+)
+
+type Token struct {
+	tokenType TokenType
+	lexeme    String
+	literal   any
+	line      int
+}
+
+func (t Token) String() string {
+	return fmt.Fprint("%s %s %s", t.tokenType, t.lexeme, t.literal)
+}
+
 type scanError struct {
 	line    int
 	where   string
